bce1f1830bc502fb72c08d0b6b8a2dd24dbd6f0c
Ann Root <ann.root@bvsd.org>
2024-10-11 11:10:41 -0600

    Create pythonlooppractice.py

---
+++ pythonlooppractice.py
@@ -0,0 +1,115 @@
+import math
+
+def assignment1(num): 
+    pass
+
+def assignment2(num):
+    pass
+
+def assignment3(num):
+    pass
+
+def assignment4(num):
+    pass
+
+def isCoolNum(n):
+    pass
+
+def assignment6(num):
+    pass
+
+def assignment7(num):
+    # print a triangle of # with num levels
+    pass
+
+def assignment8(num):
+    # print a triangle of numbers with num levels 
+    pass
+
+def assignment9(num, let):
+    # print out a letter pattern of number levels starting with letter let
+    pass
+
+print("Assignment 1 Results")
+print(assignment1(234))
+print(assignment1(10000))
+print(assignment1(111))
+print(assignment1(9005))
+print(assignment1(84645))
+print(assignment1(8547))
+print(assignment1(123456789))
+print(assignment1(2548522125455))
+print(assignment1(2545588514548))
+print(assignment1(111111111))
+print(assignment1(121212121212))
+print("Assignment 2 Results")
+print(assignment2(234))
+print(assignment2(10000))
+print(assignment2(111))
+print(assignment2(9005))
+print(assignment2(84645))
+print(assignment2(8547))
+print(assignment2(123456789))
+print(assignment2(2548522125455))
+print(assignment2(2545588514548))
+print(assignment2(111111111))
+print(assignment2(121212121212))
+print("Assignment 3 Results")
+print(assignment3(234))
+print(assignment3(10000))
+print(assignment3(111))
+print(assignment3(9005))
+print(assignment3(84645))
+print(assignment3(8547))
+print(assignment3(123456789))
+print(assignment3(2548522125455))
+print(assignment3(2545588514548))
+print(assignment3(111111111))
+print(assignment3(121212121212))
+print("Assignment 4 Results")
+print(assignment4(234))
+print(assignment4(10000))
+print(assignment4(111))
+print(assignment4(9005))
+print(assignment4(84645))
+print(assignment4(8547))
+print(assignment4(123456789))
+print(assignment4(2548522125455))
+print(assignment4(2545588514548))
+print(assignment4(111111111))
+print(assignment4(121212121212))
+print("Assignment 5: Is Cool Number")
+isCoolNum(250)
+isCoolNum(1250)
+isCoolNum(2250)
+isCoolNum(5550)
+isCoolNum(9500)
+isCoolNum(23500)
+isCoolNum(32500)
+print("Assignment 6")
+assignment6(1)
+assignment6(2)
+assignment6(2)
+assignment6(4)
+assignment6(9)
+assignment6(12)
+print("Assignment 7")
+assignment7(1)
+assignment7(2)
+assignment7(2)
+assignment7(4)
+assignment7(9)
+assignment7(12)
+print("Assignment 8")
+assignment8(1)
+assignment8(2)
+assignment8(2)
+assignment8(4)
+assignment8(9)
+assignment8(8)
+print("Assignment 9")
+assignment9(4, 'C')
+assignment9(5, 'A')
+assignment9(7, 'B')
+assignment9(7, 'X')
+assignment9(8, 'Z')
